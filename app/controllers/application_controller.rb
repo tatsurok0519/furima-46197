5e480bfe8108e3e143cbf62746499b348120d963
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-12 15:42:45 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # Userモデルにnicknameなどのカラムを追加した場合に備えて設定します。
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ryoyayuki' && password == '11223'
+    end
   end
 end
