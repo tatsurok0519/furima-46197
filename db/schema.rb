b9adb1a7c51af07b444e523161b640201ff2ea16
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-13 17:28:18 +0900

    新規登録、ログイン、ログアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_12_232759) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_13_061256) do
   create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
     t.string "postal_code", null: false
     t.integer "prefecture_id", null: false
@@ -56,6 +56,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_232759) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
