75948c9b43d35e3db65fb6b2d653e3a0bc358eb0
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-20 10:36:41 +0900

    本番用全機能確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,10 @@ class OrdersController < ApplicationController
   # ログインしていないユーザーはログインページに遷移させる
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :set_public_key, only: [:index, :create] # 公開鍵の設定を共通化
   before_action :redirect_if_seller_or_sold, only: [:index, :create]
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping_address = OrderShippingAddress.new
   end
 
@@ -14,11 +14,17 @@ class OrdersController < ApplicationController
     if @order_shipping_address.valid?
       pay_item
       @order_shipping_address.save
-      redirect_to root_path
-    else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-      render :index, status: :unprocessable_entity
+      return redirect_to root_path
     end
+
+    # バリデーションに失敗した場合はフォームを再表示
+    render :index, status: :unprocessable_entity
+  # pay_itemで発生しうるカードエラー（残高不足など）をここで捕捉
+  rescue Payjp::CardError => e
+    # エラー内容をログに記録し、ユーザーにメッセージを表示
+    Rails.logger.error "Pay.jp CardError: #{e.message}"
+    @order_shipping_address.errors.add(:token, "カード情報が正しくありません。")
+    render :index, status: :unprocessable_entity
   end
 
   private
@@ -28,17 +34,19 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    # フォームから送信される情報をフィルタリングし、必要な情報をマージ
     params.require(:order_shipping_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
   
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def set_public_key
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def redirect_if_seller_or_sold
